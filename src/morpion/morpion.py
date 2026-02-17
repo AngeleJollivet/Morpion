fa34c0717986670c8ef973edb0b6f5d8715c0515
AngeleJollivet <angelejollivet2@gmail.com>
2026-02-17 12:05:07 +0100

    ajout classes et fonctions de base

---
+++ src/morpion/morpion.py
@@ -0,0 +1,13 @@
+class Morpion : 
+    arbre = []
+    plateau = plateau()
+    arbre.append(plateau)
+    racine = arbre[0]
+    pass
+
+def plateau():
+    return [[[],[],[]], 
+            [[],[],[]],
+            []]
+
+
