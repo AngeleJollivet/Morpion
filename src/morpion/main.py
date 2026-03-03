60c8c20f0052b1eca36fedfa63c839981c418029
Juliette MARTIN <martinjuliette14@gmail.com>
2026-03-03 10:05:33 +0100

    small changes to main py

--- src/morpion/main.py
+++ src/morpion/main.py
@@ -2,8 +2,8 @@ import sys
 # Classe principale pour l'application PyQt5
 from PyQt5.QtWidgets import QApplication
 # Import de la fenêtre principale de l'application
-from View.quiz_view import QuizView
+from View.morpion_view import MorpionView
 app = QApplication(sys.argv) # Crée l'application
-window = QuizView() # Crée la fenêtre
+window = MorpionView() # Crée la fenêtre
 window.show() # Affiche la fenêtre
 sys.exit(app.exec_()) # Lance la boucle de l'application et quitte proprement
